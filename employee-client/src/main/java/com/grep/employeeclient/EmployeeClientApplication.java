7ccf343a51307d0b3b318703e305c6849d9d179d
preetham1290 <preetham1290@IP-C0A880FF.corp.amazonworkspaces.com>
2018-01-25 15:50:54 +0530

    Works with resttemplate

--- employee-client/src/main/java/com/grep/employeeclient/EmployeeClientApplication.java
+++ employee-client/src/main/java/com/grep/employeeclient/EmployeeClientApplication.java
@@ -8,14 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.hateoas.Resources;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
@@ -23,7 +29,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@EnableFeignClients
+//@EnableFeignClients
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
@@ -32,14 +38,20 @@ public class EmployeeClientApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeClientApplication.class, args);
 	}
+
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 }
 
-@FeignClient("employee-service")
-@Component
-interface EmployeeReader {
-	@GetMapping("employees")
-	Resources<Employee> getNames();
-}
+//@FeignClient("employee-service")
+//@Component
+//interface EmployeeReader {
+//	@GetMapping("employees")
+//	Resources<Employee> getNames();
+//}
 
 @Data
 @AllArgsConstructor
@@ -51,16 +63,29 @@ class Employee {
 @RestController
 @RequestMapping("employees")
 class EmployeeClient {
-	@Autowired
-	private EmployeeReader employeeReader;
+//	@Autowired
+//	private EmployeeReader employeeReader;
 
-	List<String> fallBack() {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	List<Employee> fallBack() {
 		return new ArrayList<>();
 	}
 
-	@HystrixCommand(fallbackMethod = "fallBack")
-	@GetMapping("names")
-	List<String> getNames() {
-		return employeeReader.getNames().getContent().stream().map(Employee::getName).collect(Collectors.toList());
+//	@HystrixCommand(fallbackMethod = "fallBack")
+//	@GetMapping("names")
+//	List<String> getNames() {
+//		return employeeReader.getNames().getContent().stream().map(Employee::getName).collect(Collectors.toList());
+//	}
+
+//	@HystrixCommand(fallbackMethod = "fallBack")
+	@GetMapping("restnames")
+	List<Employee> getNamesFromRest() {
+		ResponseEntity < Resources < Employee >> response = restTemplate.exchange("http://employee-service/employees",
+			    HttpMethod.GET, null, new ParameterizedTypeReference < Resources < Employee >> () {});
+		System.err.println(response.getBody().getContent().size());
+		return response.getBody().getContent().stream().collect(Collectors.toList());
 	}
+
 }
